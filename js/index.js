5ca58b98be500c25f09bfd4e3b07a640bb8e3e33
Leonel Nardelli <leonardelli@MacBook-Pro-de-Leonel.local>
2020-01-16 11:59:53 -0300

    Probando con video

---
+++ js/index.js
@@ -0,0 +1,38 @@
+$(function(){
+  var typed = new Typed('#typed', {
+  strings: ["", "Filtros personalizados", "Casamientos, Despedidas de solteros", "Logos Empresariales", "Baby showers", "Ocaciones especiales", "Soprende a tus amigos"],
+  typeSpeed: 70,
+  showCursor: false,
+  loop: true
+});
+
+
+  var vMascara = $("video")[0];
+  var vMascaraFlotante = $("video")[1];
+  var vFondo = $("video")[2];
+  var vLogo = $("video")[3];
+
+
+  $(document).on('scroll',function(){
+
+    var scrollTop = $(document).scrollTop();
+    console.log("Scroll:" + scrollTop);
+
+
+    if (scrollTop < 2) {
+      vMascara.pause();
+      vMascaraFlotante.pause();
+      vFondo.pause();
+      vLogo.pause();
+
+    } else{
+      vMascara.play();
+      vMascaraFlotante.play();
+      vFondo.play();
+      vLogo.play();
+    }
+
+  });
+
+
+});
